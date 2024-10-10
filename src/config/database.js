b0eeab3b8bc91eef585b7e825ebebe549000b7f6
SamuelRBatista <samuel.r.batista@outlook.com>
2024-10-10 13:46:30 -0300

    InitialCommit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'us-cdbr-east-02.cleardb.com',
+    username:'b025f14eee6806',
+    password:'b4a97911',
+    database:'heroku_8b11e07b9785fb1',
+    define:{
+        timestamps:true,
+        underscored:true,
+    },
+};
+
+//Este modulo e responsável por definir o banco de dados e o endereço a senha e o nome de banco de dados..
